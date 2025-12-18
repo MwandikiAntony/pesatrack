0a3e7bbf3c81103a17596e900f452ba536c600ed
MwandikiAntony <antonymwandiki26@gmail.com>
2025-12-18 08:39:59 +0300

    added expense model

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'models/expense.dart';
 import 'screens/main_layout.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Hive.initFlutter();
+  Hive.registerAdapter(ExpenseAdapter());
+  await Hive.openBox<Expense>('expenses');
+
   runApp(const PesaTrackApp());
 }
 
